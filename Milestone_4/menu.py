64c7fa545506f13cc3847c3306628934e70321ba
AdieStevens <adie.stevens@mymail.champlain.edu>
2024-09-25 10:36:26 -0400

    Updated menu

--- Milestone_4/menu.py
+++ Milestone_4/menu.py
@@ -4,4 +4,23 @@ def menu():
     print("[3] VM Details")
     print("[0] Exit")
 
+menu()
+option = int(input("Enter Option Number:"))
 
+while option != 0:
+    if option == 1:
+        # Option 1 function here
+        print("option 1")
+    elif option == 2:
+        # Option 2 function here
+        print ("option 2")
+    elif option == 3:
+        #option 3 function here
+        print ("option 3")
+    else:
+        print("Invalid Option - Please choose from the list above")
+
+    menu()
+    option = int(input("Enter Option Number:"))
+
+print ("Deuces")
