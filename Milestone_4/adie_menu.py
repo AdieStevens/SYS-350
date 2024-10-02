b2fc5be6f0dd34d874f36f1672a180f5c997e06b
AdieStevens <adie.stevens@mymail.champlain.edu>
2024-10-02 11:36:52 -0400

    Finish

--- Milestone_4/adie_menu.py
+++ Milestone_4/adie_menu.py
@@ -112,35 +112,3 @@ def menu():
 
 if __name__ == "__main__":
     menu()
-
-
-
-
-
-'''
-    print("[1] Vcenter Info ")
-    print("[2] Session Details")
-    print("[3] VM Details")
-    print("[0] Exit")
-
-menu()
-option = int(input("Enter Option Number:"))
-
-while option != 0:
-    if option == 1:
-        # Option 1 function here
-        print("option 1")
-    elif option == 2:
-        # Option 2 function here
-        print ("option 2")
-    elif option == 3:
-        #option 3 function here
-        print ("option 3")
-    else:
-        print("Invalid Option - Please choose from the list above")
-
-    menu()
-    option = int(input("Enter Option Number:"))
-
-print ("Deuces")
-'''
